abbfec7f17de37ff20af03d4af146e84c68e1e96
Cristina Scognamiglio <77617593+CristinaSco@users.noreply.github.com>
2021-03-22 19:10:47 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+
+var x = document.getElementById("tanya");
+var y = document.getElementById("john");
+y.style.display = "none";
+
+function prev() {
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
+
+function next() {
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
